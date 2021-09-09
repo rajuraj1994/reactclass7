e3d72285b22e6fa3531fa268a923dc90edb1d77c
rajuraj1994 <rajurajkandel@gmail.com>
2021-09-09 14:21:19 +0545

    payment process

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useEffect} from 'react'
+import {API} from '../config'
 import Nav from '../components/Nav'
 import Footer from '../components/Footer'
 import { withRouter } from 'react-router-dom'
@@ -8,6 +9,8 @@ import CheckoutStep from '../components/CheckoutStep'
 import axios from 'axios'
 import { isAuthenticated } from './auth'
 import { useStripe,useElements,CardNumberElement,CardExpiryElement,CardCvcElement } from '@stripe/react-stripe-js'
+import { toast,ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 
 const options={
     style:{
@@ -24,15 +27,76 @@ const Payment = ({history}) => {
     const stripe=useStripe()
     const elements=useElements()
     const dispatch=useDispatch()
-    const{user}=isAuthenticated()
+    const{user,token}=isAuthenticated()
     const{cartItems,shippingInfo}=useSelector(state=>state.cart)
+
+    useEffect(()=>{
+
+    },[])
+    const orderInfo=JSON.parse(sessionStorage.getItem('orderInfo'))
+
+    const paymentData={
+        amount:Math.round(orderInfo.totalPrice*100)
+    }
+
+    const submitHandler=async (e)=>{
+      e.preventDefault();
+      document.querySelector('#pay_btn').disabled=true
+      let res
+      try{
+          const config={
+              headers:{
+                  "Content-Type":'application/json',
+                   Authorization:`Bearer ${token}`
+              }
+          }
+          res=await axios.post(`${API}/payment/process`,paymentData,config)
+
+          const client_secret=res.data.client_secret
+          console.log(client_secret)
+
+          if(!stripe || !elements){
+              return
+          }
+          const result=await stripe.confirmCardPayment(`${client_secret}`,{
+              payment_method:{
+                  card:elements.getElement(CardNumberElement),
+                  billing_details:{
+                      name:user.name,
+                      email:user.email
+                  }
+              }
+          })
+          if(result.error){
+              toast.error(result.error.message)
+              document.querySelector('#pay_btn').disabled=false
+              
+
+          }
+          else{
+              //payment is processsed or not
+              if(result.paymentIntent.status==='succeeded'){
+                  history.push('/success')
+              }
+              else{
+                  toast.error('There is some error while processing')
+              }
+          }
+
+      }
+      catch(error){
+          document.querySelector('#pay_btn').disabled=false
+          toast.error(error.message)
+      }
+    }
     return (
         <>
       <Nav/>
+      <ToastContainer theme="colored"/>
         <div className="container">
                  <CheckoutStep shipping confirmOrder payment/>
                 <div className="col-md-5 offset-md-3 p-3 mb-3 mt-4 shadow-lg">
-                    <form>
+                    <form  onSubmit={submitHandler}>
                         <h2 className="mb-3">Card Information </h2>
                         <div className="col-12 mb-3">
                             <label htmlFor="card-number">Card Number</label>
@@ -54,9 +118,8 @@ const Payment = ({history}) => {
                         </div>
                         
                         <div className="col-12 mb-3">
-                            <button className="btn btn-warning form-control"
-                            onClick=""
-                            >Pay</button>
+                            <button className="btn btn-warning form-control" id="pay_btn"
+                            >Pay{`-${orderInfo&& orderInfo.totalPrice}`}</button>
                         </div>
                     </form>
 
